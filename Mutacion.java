c4c24a49412052fa9e6a32e973d23d3fdcfdec6e
PieroCM <pierojesus_20@hotmail.com>
2025-06-23 17:40:22 -0500

    Cambie la parte de Mutacion y de Poblacion el problema del N

--- Mutacion.java
+++ Mutacion.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
+
 public class Mutacion {
+private static int N;
     
+/* 
     public static int[][] NewCromosomas (int[][] Puntajes, int[][] MatrizCromosomas){
         //scores habran 0 , 3
         //MatrizCromo 2 , 3
-        int[][] NewCromosomas = new int[MatrizCromosomas.length][MatrizCromosomas[0].length];
+        Integer[][] NewCromosomas = new Integer[MatrizCromosomas.length][MatrizCromosomas[0].length];
         for(int i = 0 ; i<MatrizCromosomas.length; i++){
             for(int j = 0 ; j<MatrizCromosomas[i].length ; j++){
-                    if(Puntajes[i][j] == 0){
-                        MatrizCromosomas 
+                    if(Puntajes[i][j] == 1){
+                        
                     }else{
 
                     }
@@ -16,21 +20,20 @@ public class Mutacion {
         
 
 
-            
+
         }
-
-
-    
-
-
-
-
-
-
+*/
     public static void main (String[] args){
-
-
-
+        CrearSudoku sudoku = new CrearSudoku();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Tamaño del Sudoku (ej: 4, 9, 16): ");
+        N = scanner.nextInt();
+        Integer[][] MSudoku = sudoku.generarSudoku(N);
+        sudoku.imprimirTablero(MSudoku);
+        Poblacion poblacion = new Poblacion();
+        int[][] MPoblacion = poblacion.CrearPoblacion(MSudoku,N);
+        poblacion.imprimirMatriz(MPoblacion);
+        scanner.close();
     }
 
 
--- Poblacion.java
+++ Poblacion.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class Poblacion {
     
-private static int [][] Mcromosomas;
-private static int N;
 
-public static int[][] CrearPoblacion(Integer[][] matriz){
+
+
+public Poblacion(){
+
+}
+
+
+public  int[][] CrearPoblacion(Integer[][] matriz, int N){
         Scanner scanner = new Scanner(System.in);
         int NespacioVacio = 0;
         System.out.print("¿Ingrese el numero de cromosomas que desea? ");
         int Ncromosomas = scanner.nextInt();
-
         for(int i = 0 ; i<N ; i++){
             for(int j = 0 ; j<N ; j++){
                 if(matriz[i][j] == null){
@@ -21,12 +25,12 @@ public static int[][] CrearPoblacion(Integer[][] matriz){
             }
         }
         int[][] cromosomas = new int[Ncromosomas][NespacioVacio];
-        Mcromosomas = CrearMatrizCromosomas(cromosomas);
+        int [][]Mcromosomas = CrearMatrizCromosomas(cromosomas,N);
         System.err.println(NespacioVacio);
         scanner.close();    
         return Mcromosomas;
     }
-public static int [][] CrearMatrizCromosomas(int[][] cromosomas){
+public  int [][] CrearMatrizCromosomas(int[][] cromosomas,int N){
     Random rand = new Random();
     for(int i = 0 ; i<cromosomas.length; i++){
             for(int j = 0 ; j<cromosomas[i].length ; j++){
@@ -36,10 +40,10 @@ public static int [][] CrearMatrizCromosomas(int[][] cromosomas){
             return cromosomas;
         }
 
-     public static void imprimirMatriz(int[][] matriz) {
-        for (int[] fila : matriz) {
-            for (int valor : fila) {
-                     System.out.printf("[%3d] ", valor);
+public  void imprimirMatriz(int[][] matriz) {
+    for (int[] fila : matriz) {
+        for (int valor : fila) {
+            System.out.printf("[%3d] ", valor);
                 
             }
             System.out.println();
@@ -57,6 +61,8 @@ public static int [][] Update_Poblacion(int[][] MPoblacion){
 }
         */
     
+
+        /* 
 public static void main(String[] args){
     CrearSudoku sudoku = new CrearSudoku();
     Scanner scanner = new Scanner(System.in);
@@ -70,5 +76,5 @@ public static void main(String[] args){
 
 }
 
-
+*/
 }

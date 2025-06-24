5fc5ebadbad1e37819e18efffaf9504696d6a366
PieroCM <pierojesus_20@hotmail.com>
2025-06-23 19:18:40 -0500

    LOL MAIN REVISA

--- CrearSudoku.java
+++ CrearSudoku.java
@@ -91,15 +91,5 @@ public class CrearSudoku {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Tamaño del Sudoku (ej: 4, 9, 16): ");
-        int n = scanner.nextInt();
-
-        CrearSudoku generador = new CrearSudoku();
-        Integer[][] sudoku = generador.generarSudoku(n);
-
-        System.out.println("\nSudoku");
-        generador.imprimirTablero(sudoku);
-    }
+  
 }
